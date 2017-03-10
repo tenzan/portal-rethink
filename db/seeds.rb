cc7af8ea210c8816784159c55f717926942cc3c1
Askar Karasaev <askar.karasaev@gmail.com>
2017-03-10 14:50:03 +0900

    Adding initial sample data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Site.create!('code': 111,'name':'Site 1')
+Site.create!('code': 222,'name':'Site 2')
+Site.create!('code': 333,'name':'Site 3')
+Site.create!('code': 444,'name':'Site 4')
+
+Type.create!('name':'Type 1')
+Type.create!('name':'Type 2')
+Type.create!('name':'Type 3')
+Type.create!('name':'Type 4')
